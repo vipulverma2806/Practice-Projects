317beeeb26d2586fb5c786b892e91062f2dcd05e
Vipul Verma <56539924+vipulverma2806@users.noreply.github.com>
2025-01-14 23:18:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Counter from "./Counter";
 import Api from "./Api";
 import Todo from "./todo";
 import Bg from "./Bg";
-
 import Weather from "./weather";
 import SearchBar from "./SearchBar";
 import Page from "./Page";
@@ -24,13 +23,13 @@ import Digiclock from "./Digiclock";
 import Tooltip from "./Tooltip/Tooltip";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./RouterApp/Router";
-
 import Quotes from "./Quote/Quotes";
 import UseFetchHookTest from "./CustomHooks/useFetchTest";
 import ScrollToTopAndBottom from "./Scroll/scroll-to-top-and-bottom";
 import Progress from "./progress-bar/Progress";
 import StarRating from "./Star/index";
 import BMICalculator from "./BMI/index";
+import TabTest from "./custom-tabs/tab-test";
 
 const App = () => {
   const items = [
@@ -47,40 +46,38 @@ const App = () => {
 
   return (
     <div>
-      {/* <One></One> */}
-      {/* <Two></Two> */}
-      {/* <Three/> */}
-      {/* <Four></Four> */}
-      {/* <Five></Five> */}
-      {/* <Six/> */}
-      {/* <Seven></Seven> */}
-      {/* <Counter/>8 */}
+      {/* 1<One></One> */}
+      {/* 2<Two></Two> */}
+      {/* 3<Three/> */}
+      {/* 4<Four></Four> */}
+      {/* 5<Five></Five> */}
+      {/* 6<Six/> */}
+      {/* 7<Seven></Seven> */}
+      {/* 8<Counter/>8 */}
       {/* 9<Api></Api> */}
       {/* 10 <Todo></Todo> */}
       {/* 11<Bg></Bg> */}
-
-      {/* 13<Weather></Weather> */}
-      {/* 14<SearchBar items={items}></SearchBar> */}
-      {/* 15 <Page items={items} perPage={2} ></Page> */}
-      {/* 16<Pickc></Pickc> */}
-      {/* 17<Hamicon></Hamicon> */}
-      {/* 18 <Theme></Theme> not completed */}
-      {/* 19<CountRedu></CountRedu> */}
-      {/* 20<Multistate></Multistate> */}
-      {/* 21<Image></Image> */}
-      {/* 22<Digiclock></Digiclock> */}
-      {/* 23<Tooltip></Tooltip> */}
-
-      {/* 24<BrowserRouter>
+      {/* 12<Weather></Weather> */}
+      {/* 13<SearchBar items={items}></SearchBar> */}
+      {/* 14 <Page items={items} perPage={2} ></Page> */}
+      {/* 15<Pickc></Pickc> */}
+      {/* 16<Hamicon></Hamicon> */}
+      {/* 17 <Theme></Theme> not completed */}
+      {/* 18<CountRedu></CountRedu> */}
+      {/* 19<Multistate></Multistate> */}
+      {/* 20<Image></Image> */}
+      {/* 21<Digiclock></Digiclock> */}
+      {/* 22<Tooltip></Tooltip> */}
+      {/* 23<BrowserRouter>
         <Router></Router>
       </BrowserRouter> */}
-
-      {/* 25 <Quotes></Quotes> */}
-      {/* 26<UseFetchHookTest></UseFetchHookTest> */}
-      {/* 27<ScrollToTopAndBottom>27</ScrollToTopAndBottom> */}
-      {/* 28<Progress>28</Progress> */}
-      {/* 29<StarRating></StarRating> */}
-        <BMICalculator></BMICalculator>
+      {/* 24 <Quotes></Quotes> */}
+      {/* 25<UseFetchHookTest></UseFetchHookTest> */}
+      {/* 26<ScrollToTopAndBottom></ScrollToTopAndBottom> */}
+      {/* 27<Progress></Progress> */}
+      {/* 28<StarRating></StarRating> */}
+      {/* 29<BMICalculator></BMICalculator> */}
+      30 <TabTest></TabTest>
     </div>
   );
 };
